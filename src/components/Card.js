6480a093816157785c3a6f1844f85072c2f98e72
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-08-10 05:47:38 +0000

    Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <>
             <div className="col-md-4 col-10 mx-auto">
-                <div className="card h-100">
+                <div className="card w-100 h-100">
                     <img src={props.imgsrc} class="card-img-top" alt={props.imgsrc} width="250px" height="190px" />
                     <div className="card-body">
                         <h5 className="card-title font-weight-bold">{props.title}</h5>
